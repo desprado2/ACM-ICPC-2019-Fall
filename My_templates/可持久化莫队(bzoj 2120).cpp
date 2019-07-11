d72effe9254939b8eed5e7133eb577ff9672c922
desprado2 <46255627+desprado2@users.noreply.github.com>
2019-07-11 22:54:57 +0800

    n^(2/3) per block, n^(1/3) blocks in total.

--- My_templates/可持久化莫队(bzoj 2120).cpp
+++ My_templates/可持久化莫队(bzoj 2120).cpp
@@ -15,7 +15,7 @@ int main()
 {
 	cin>>n>>m;
 	unit=int(pow(n,0.666));
-	for (i=1; i<=n; i++) {cin>>col[i];init[i]=col[i];}
+	for (i=1; i<=n; i++) cin>>col[i], init[i]=col[i];
 	for (i=1; i<=n; i++) bl[i]=(i-1)/unit+1;
 
 	char ch;
@@ -24,15 +24,27 @@ int main()
 	{
 		cin>>ch>>x>>y;
 		if (ch=='Q') cnt2++,q[cnt2].l=x,q[cnt2].r=y,q[cnt2].tim=cnt1,q[cnt2].id=cnt2;
-			else cnt1++,c[cnt1].pos=x,c[cnt1].t=y;
+		else cnt1++,c[cnt1].pos=x,c[cnt1].t=y;
 	}
 	sort(q+1,q+cnt2+1,cmp);
 
 	int L=1,R=0,T=0;
 	for (i=1; i<=cnt2; i++)
 	{
-		while (T>q[i].tim) {p=c[T].pos; if (p>=L&&p<=R)chk(--cnt[col[p]],'M'); col[p]=init[p]; if (p>=L&&p<=R)chk(++cnt[col[p]],'P'); T--;}
-		while (T<q[i].tim) {T++; p=c[T].pos; if (p>=L&&p<=R)chk(--cnt[col[p]],'M'); col[p]=c[T].t; if (p>=L&&p<=R)chk(++cnt[col[p]],'P');}
+		while (T>q[i].tim) {
+			p=c[T].pos;
+			if (p>=L&&p<=R) chk(--cnt[col[p]],'M');
+			col[p]=init[p];
+			if (p>=L&&p<=R) chk(++cnt[col[p]],'P');
+			T--;
+		}
+		while (T<q[i].tim) {
+			T++;
+			p=c[T].pos;
+			if (p>=L&&p<=R) chk(--cnt[col[p]],'M');
+			col[p]=c[T].t;
+			if (p>=L&&p<=R) chk(++cnt[col[p]],'P');
+		}
 
 		while (L<q[i].l) {chk(--cnt[col[L]],'M'); L++;}
 		while (L>q[i].l) {L--; chk(++cnt[col[L]],'P');}
