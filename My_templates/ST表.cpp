670eb52de2d739e3ba69ca4f551ca053e0460d64
desprado2 <46255627+desprado2@users.noreply.github.com>
2019-07-11 09:28:33 +0800

    It can be used to calculate LCA and so on.

---
+++ My_templates/ST表.cpp
@@ -0,0 +1,15 @@
+int Log2[N],st[N][20];//ST table
+void init(){
+    for (i=1,k=0; i<=n; i<<=1,k++)
+        for (j=i; j<i<<1 && j<=n; j++)
+            Log2[j]=k;
+    for (i=1; i<=n; i++) st[i][0]=rec[i];
+    for (j=1; j<=Log2[n]; j++)
+        for (i=1; i<=n; i++)
+            if (i+(1<<j-1)<=n) st[i][j]=max(st[i][j-1],st[i+(1<<j-1)][j-1]);
+            else st[i][j]=st[i][j-1];
+}
+int Q(int l,int r){
+    int len=r-l+1;
+    return max(st[l][Log2[len]] , st[r-(1<<Log2[len])+1][Log2[len]]);
+}
