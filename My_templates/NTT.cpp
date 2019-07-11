e08b319eebefbb9a86b8251c5175c7a144120f45
desprado2 <46255627+desprado2@users.noreply.github.com>
2019-07-11 09:39:40 +0800

    No comment

--- My_templates/NTT.cpp
+++ My_templates/NTT.cpp
@@ -17,11 +17,8 @@ const ll G=3;
 ll qp(ll x,ll y){
     if (!y) return 1;
     ll ret=qp(x,y>>1);
-    return (y&1)?ret*ret%M*x%M:ret*ret%M;
+    return (y&1)? ret*ret%M*x%M: ret*ret%M;
 }
-int n,i,j,k;
-ll f[N<<1],g[N<<1],fac[N<<1],ifac[N<<1];
-
 void NTT(ll* a,ll* p,int n,bool INV)
 {
     int i,j,k,rev;
@@ -48,7 +45,7 @@ void NTT(ll* a,ll* p,int n,bool INV)
     if (INV)
         for (i=0; i<n; i++) p[i]=p[i]*in%M;
 }
-int p2(int x){
+int p2(int x){//找到2^i>=x的最小i
     for (int i=1; ; i<<=1)
         if (i>=x) return i;
 }
@@ -58,30 +55,4 @@ void mul(ll* A,ll* B,int bit)//A <- A*B
     NTT(A,pA,bit,false), NTT(B,pB,bit,false);
     for (int i=0; i<bit; i++) pA[i]=pA[i]*pB[i]%M;
     NTT(pA,A,bit,true);
-}
-void calc(int l,int r){
-    int i,j,k,mid=(l+r)/2;
-    if (l==r) return;
-    calc(l,mid);
-
-    int len=p2(r-l+1);
-    for (i=0; i<len; i++) A[i]=B[i]=0;
-    for (i=l; i<=mid; i++) A[i-l]=f[i]*ifac[i]%M;
-    for (i=0; i<len; i++) B[i]=1ll*i*i%M;
-    mul(A,B,len);
-    for (i=mid+1; i<=r; i++)
-        (f[i]+=A[i-l]*fac[i-1]%M) %=M;
-
-    calc(mid+1,r);
-}
-int main()
-{
-    ios::sync_with_stdio(false);
-    for (i=1,fac[0]=ifac[0]=1; i<=N-5; i++)
-        fac[i]=fac[i-1]*i%M, ifac[i]=qp(fac[i],M-2);
-    for (i=1; i<=N-5; i++)
-        g[i]=1ll*i*i%M*ifac[i-1]%M;//dbg();
-    f[0]=1, calc(0,N-5);
-    int n;
-    while (cin>>n) cout<<f[n]<<endl;
 }
