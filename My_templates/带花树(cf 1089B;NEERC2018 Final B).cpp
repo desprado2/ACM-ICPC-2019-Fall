d7b473bed47c2b1931de58d8d4899eb5b64dd838
desprado2 <46255627+desprado2@users.noreply.github.com>
2019-07-12 00:05:53 +0800

    No comment

--- My_templates/带花树(cf 1089B;NEERC2018 Final B).cpp
+++ My_templates/带花树(cf 1089B;NEERC2018 Final B).cpp
@@ -1,11 +1,6 @@
-#include<bits/stdc++.h>
-#define N 505
-using namespace std;
-
 int n,m,i,j,k,fa[N],match[N],pre[N];//fa:disjoint set
 int cnt=0,last[N];
 struct edg{int to,next;} e[N*N*5];
-
 void adde(int a,int b)
 {
 	e[cnt].to=b,e[cnt].next=last[a],last[a]=cnt; cnt++;
@@ -13,7 +8,7 @@ void adde(int a,int b)
 }
 
 int find(int x){return (x==fa[x])?x:(fa[x]=find(fa[x]));}
-int V,tag=0,bl[N];//bl[]:belong to which cycle. 
+int V,tag=0,bl[N];//bl[]:belong to which cycle.
 int lca(int x,int y)
 {
 	tag++;
@@ -25,24 +20,36 @@ int lca(int x,int y)
 			if (bl[x]==tag) return x;
 			bl[x]=tag;
 			if (match[x]) x=find(pre[match[x]]);
-				else x=0;//ÏÈµ½ÁËÒÔºó¾Í²»ÓÃÔÙ×ßÁË 
+			else x=0;//å…ˆåˆ°äº†ä»¥åå°±ä¸ç”¨å†èµ°äº†
 		}
 		swap(x,y);
 	}
 	return x;
 }
+int state[N];
+list<int> qu;
+void shrink(int x,int lastx,int torus){
+	int temp;
+	while (find(x)!=torus){
+		pre[x]=lastx;
+		temp=match[x];
+		if (state[temp]==1) state[temp]=0,qu.push_back(temp);
+		if (find(temp)==temp) fa[temp]=torus;
+		if (find(x)==x) fa[x]=torus;
+		lastx=temp, x=pre[lastx];
+	}
+}
 bool bfs(int x)
 {
-	int i,j,u,v,p,q,qu[N],state[N];//state=-1:unvisited,0:even(state S),1:odd(state T); 
-	memset(state,-1,sizeof(state));
+	int i,u,v;
+	memset(state,-1,sizeof(state));//state=-1:unvisited,0:even(state S),1:odd(state T);
 	for (i=1; i<=V; i++) fa[i]=i;
-	
-	p=0,q=1;
-	qu[q]=x,state[x]=0;
-	while (p<q)
+
+	qu.clear(), qu.push_back(x);
+	state[x]=0;
+	while (!qu.empty())
 	{
-		p++;
-		u=qu[p];
+		u=qu.front(), qu.pop_front();
 		for (i=last[u]; i!=-1; i=e[i].next)
 		{
 			v=e[i].to;
@@ -56,68 +63,27 @@ bool bfs(int x)
 					{
 						temp=pre[now];
 						int next=match[temp];
-						match[temp]=now,match[now]=temp;
+						match[temp]=now, match[now]=temp;
 						now=next;
 					}
 					return 1;
 				}
-				state[match[v]]=0; qu[++q]=match[v];
+				state[match[v]]=0; qu.push_back(match[v]);
 			}
-			else if (state[v]==0){//ÖØĞÂ·ÃÎÊµ½ÒÑÓĞµÄµã£¬±ØÈ»ÊÇÆæ»·
+			else if (state[v]==0){//é‡æ–°è®¿é—®åˆ°å·²æœ‰çš„ç‚¹ï¼Œå¿…ç„¶æ˜¯å¥‡ç¯
 				int torus=lca(u,v);
-				//Ëõµã 
-				int x=u,lastx=v,temp;
-				while (find(x)!=torus)
-				{
-					pre[x]=lastx;
-					temp=match[x];
-					if (state[temp]==1) state[temp]=0,qu[++q]=temp;
-					if (find(temp)==temp) fa[temp]=torus;
-					if (find(x)==x) fa[x]=torus;
-					lastx=temp,x=pre[lastx];
-				}
-				x=v,lastx=u;
-				while (find(x)!=torus)
-				{
-					pre[x]=lastx;
-					temp=match[x];
-					if (state[temp]==1) state[temp]=0,qu[++q]=temp;
-					if (find(temp)==temp) fa[temp]=torus;
-					if (find(x)==x) fa[x]=torus;
-					lastx=temp,x=pre[lastx];
-				}
+				shrink(u,v,torus), shrink(v,u,torus);//ç¼©ç‚¹
 			}
 		}
 	}
 	return 0;
 }
-int bst()
+int bst()//è®°å¾—èµ‹å€¼æ€»ç‚¹æ•°V
 {
 	memset(match,0,sizeof(match)),memset(pre,0,sizeof(pre));
 	memset(bl,0,sizeof(bl)),tag=0;
-	int ans=0;
+	int ret=0;
 	for (int i=1; i<=V; i++)
-		if (!match[i]) ans+=bfs(i);
-	return ans;
-}
-int main()
-{
-	int t;
-	cin>>t;
-	while (t--)
-	{
-		cin>>n>>m;
-		V=n*2+m;
-		memset(last,-1,sizeof(last)),cnt=0;
-		for (i=1; i<=n; i++)
-		{
-			string s;
-			cin>>s;
-			for (j=0; j<m; j++)
-				if (s[j]=='1') adde(i,n*2+j+1),adde(i+n,n*2+j+1);
-		}
-		for (i=1; i<=n; i++) adde(i,i+n);
-		
-		cout<<bst()-n<<endl;
-	}
+		if (!match[i]) ret+=bfs(i);
+	return ret;
 }
