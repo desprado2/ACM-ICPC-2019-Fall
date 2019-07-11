377fae115e7ea238145632406a8e160139ade0ae
desprado2 <46255627+desprado2@users.noreply.github.com>
2019-07-11 10:31:37 +0800

    No comment

--- My_templates/spfa(SLF优化)+最大费用最大流.cpp
+++ My_templates/spfa(SLF优化)+最大费用最大流.cpp
@@ -15,13 +15,13 @@ void adde(int fr,int to,int val,int c)
 	e[cnt].next=l[to],e[cnt].to=fr,e[cnt].val=0,e[cnt].c=-c; l[to]=cnt; cnt++; //-c !!!
 }
 int pre[N],id[N],dis[N];
-deque<int> qu;
-int spfa(int st,int en)//spfa增广
+list<int> qu;
+int spfa(int st,int en)//spfa澧炲箍
 {
 	int i,j,p=0,q=1,ret=inf;
 	bool vis[N];
 
-	qu.clear(); memset(vis,0,sizeof(vis)); memset(dis,-1,sizeof(dis));//是-1，不是0!!
+	qu.clear(); memset(vis,0,sizeof(vis)); memset(dis,-1,sizeof(dis));
 	qu.push_front(st); dis[st]=0,pre[st]=-1,id[st]=-1; vis[st]=1;
 	while (!qu.empty())
 	{
@@ -37,8 +37,8 @@ int spfa(int st,int en)//spfa增广
 				pre[v]=u,id[v]=i;
 				if (!vis[v])
 				{//SLF optimization
-					if (qu.empty()||dis[v]>dis[qu.front()]) qu.push_front(v);//注意判断empty!!!!!!!
-						else qu.push_back(v);
+					if (qu.empty()||dis[v]>dis[qu.front()]) qu.push_front(v);
+					else qu.push_back(v);
 					vis[v]=1;
 				}
 			}
@@ -49,46 +49,17 @@ int spfa(int st,int en)//spfa增广
 
 	i=en;
 	do
-	{
-		ret=min(ret,e[id[i]].val);
-		i=pre[i];
-	}while (i!=st);
+		ret=min(ret,e[id[i]].val), i=pre[i];
+	while (i!=st);
+	
 	i=en;
 	do
-	{
-		e[id[i]].val-=ret,e[id[i]^1].val+=ret;
-		i=pre[i];
-	}while(i!=st);
+		e[id[i]].val-=ret, e[id[i]^1].val+=ret, i=pre[i];
+	while(i!=st);
 	return ret*dis[en];
 }
-int main()
-{
-	cin>>t;
-	while (t--)
-	{
-		memset(l,-1,sizeof(l)); cnt=ans=0;
-		cin>>m>>n>>k>>w;
-		for (i=1; i<=n; i++)
-			cin>>a[i].l>>a[i].r>>a[i].w>>a[i].op;
-
-		S=n*2+1,T=n*2+2;
-		for (i=1; i<=n; i++)
-		{
-			adde(i,i+n,1,0);
-			adde(S,i,1,a[i].w); adde(i+n,T,1,0);
-			for (j=1; j<=n; j++)
-			{
-				if (j==i) continue;
-				if (a[i].r<=a[j].l)
-					if (a[i].op==a[j].op) adde(i+n,j,inf,a[j].w-w);
-					else adde(i+n,j,inf,a[j].w);
-			}
-		}
-		SS=n*2+3; adde(SS,S,k,0);
-
-		tmp=spfa(SS,T);
-		while (tmp>0) {ans+=tmp; tmp=spfa(SS,T);}
-		cout<<ans<<endl;
-	}
-	return 0;
+int work(int st,int en){
+	int tmp, ret=0;
+	while ((tmp=spfa(st,en))>0) ret+=tmp;
+	return ret;
 }
