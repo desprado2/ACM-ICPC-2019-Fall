2c1979685c5a3e68e06a852a4a003d8489629a07
desprado2 <46255627+desprado2@users.noreply.github.com>
2019-07-11 10:18:12 +0800

    Please use it with notes

--- My_templates/exgcd.cpp
+++ My_templates/exgcd.cpp
@@ -1,14 +1,5 @@
-#include<bits/stdc++.h>
-typedef long long LL;
-using namespace std;
-
-void exgcd(LL a,LL b,LL &x,LL &y){
+void exgcd(LL a,LL b,LL &x,LL &y){// 求ax+by=gcd(a,b)的一个特解(x,y)
 	if (b==0) {x=1,y=0; return;}
 	exgcd(b,a%b,x,y);
 	LL t=x; x=y, y=t-(a/b)*y;
-}
-LL inv(LL a){
-    LL x,y;
-    exgcd(a,M,x,y);
-    return (x+M)%M;
 }
