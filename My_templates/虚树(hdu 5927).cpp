68ec93cd96e333670d0e0c04724709015fb94d74
desprado2 <46255627+desprado2@users.noreply.github.com>
2019-07-13 00:36:33 +0800

    Please use it with notes

--- My_templates/虚树(hdu 5927).cpp
+++ My_templates/虚树(hdu 5927).cpp
@@ -1,21 +1,5 @@
-#include <bits/stdc++.h>
-#pragma GCC optimize(3)
-#define pb(a) push_back(a)
-#define mp(a,b) make_pair(a,b)
-#define mem(a,b) memset(a,b,sizeof(a))
-#define dbg() system("pause")
-#define clr(a,n)\
- for(int i=0; i<n; i++)a[i].clear();
-#define N 100005
-#define root 1
-using namespace std;
 
-typedef long long ll;
-typedef pair<int,int> pii;
-
-int n,m,q,i,j,k;
-vector<int> e[N];
-
+vector<int> e[N];//e是原来的树
 int dfn[N],fa[N][17],dep[N],sz[N],deg[N],tag;
 void dfs(int u,int p,int d){
     dfn[u]=++tag, dep[u]=d, fa[u][0]=p;
@@ -39,69 +23,38 @@ int lca(int u,int v){
     return fa[u][0];
 }
 
-vector<int> id,T[N];
+vector<int> id,T[N];//T存储虚树
 bool cmp(int a,int b){return dfn[a]<dfn[b];}
 
-bool imp[N];
-int csz[N],ans;
-void tra(int u,int p){
-    int cnt=0;
-    csz[u]=imp[u];
-    for (int v:T[u]){
-        tra(v,u), csz[u]+=csz[v];
-        if (fa[v][0]==u && csz[v]==sz[v]) cnt++;
-    }
-    if (deg[u]-cnt<2 && imp[u]) ans++;
-}
-int main()
-{
-    cin.tie(0), ios::sync_with_stdio(false);
-    int cases; cin>>cases;
-    for (int tt=1; tt<=cases; tt++){
-        cin>>n>>q;
-        clr(e,n+5);
-        for (i=1; i<n; i++){
-            int u,v; cin>>u>>v;
-            e[u].pb(v), e[v].pb(u);
+void work(){//事实上
+    tag=0, dfs(root,root,1), getst();
+    while (Q--){
+        cin>>m, id.clear();
+        for (int i=1; i<=m; i++){
+            int u; cin>>u;//u是每次询问的“有效节点”
+            id.pb(u);
         }
-        tag=0, dfs(root,root,1), getst();
+        sort(id.begin(), id.end(), cmp);
+        for (int i=0, j=id.size(); i+1<j; i++)
+            id.pb(lca(id[i],id[i+1]));
+        sort(id.begin(), id.end(), cmp);
+        auto it=unique(id.begin(), id.end());
+        id.erase(it,id.end());
 
-        cout<<"Case #"<<tt<<":\n";
-        while (q--){
-            cin>>m;
-            //��������
-            id.clear();
-            for (i=1; i<=m; i++){
-                int u; cin>>u;
-                id.pb(u), imp[u]=1;
-            }
-            sort(id.begin(), id.end(), cmp);
-            for (i=0, j=id.size(); i+1<j; i++)
-                id.pb(lca(id[i],id[i+1]));
-            sort(id.begin(), id.end(), cmp);
-            auto it=unique(id.begin(), id.end());
-            id.erase(it,id.end());
-
-            stack<int> S;
-            S.push(id[0]);
-            for (i=1; i<id.size(); i++){
-                int u=id[i];
-                while (!S.empty()){
-                    int v=S.top();
-                    if (lca(u,v)==v){
-                        T[v].pb(u); break;
-                    }
-                    S.pop();
+        stack<int, vector<int>> S;
+        S.push(id[0]);
+        for (int i=1; i<id.size(); i++){
+            int u=id[i];
+            while (!S.empty()){
+                int v=S.top();
+                if (lca(u,v)==v){
+                    T[v].pb(u); break;
                 }
-                S.push(u);
+                S.pop();
             }
-            //�������
-            ans=0, tra(id[0],-1);
-            cout<<n-ans<<endl;
-
-            for (int i:id)
-                T[i].clear(), csz[i]=0, imp[i]=0;
+            S.push(u);
         }
+        doCalculate();//虚树构建完成，根据题目要求完成计算
+        for (int i:id) T[i].clear();//注意清空虚树
     }
-    return 0;
 }
